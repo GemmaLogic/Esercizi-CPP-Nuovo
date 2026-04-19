9524c48d869f5c31b4fab800535fc9e6cc49649d
GemmaLogic <lorenzoguts96@gmail.com>
2026-04-19 16:44:36 +0200

    Finito e archiviato esercizio 10 Capitolo 5
    Terminato l'esercizio e archiviato esercizio numero 10

--- Capitolo5/Esercizio10.cpp
+++ Capitolo5/Esercizio10.cpp
@@ -9,15 +9,15 @@
 using namespace std;
 
 void error(string s);
-double somma_num(vector<double> n, int quanti_num);
-
+double somma_num(const vector<double>& n, int quanti_num);
+void calcola_diff(const vector<double>& n, vector<double>& diff);
 int main(){
     
     try{
         int quanti_num = 0; // var dove salviamo quanti numeri del vector sommare
         double numero = 0; // input numero
         vector<double> numeri; // vector dove verranno salvati i numeri
-        
+        vector<double> differenze; // salveremo le differenze come richiesto dall'esercizio
         cout << "Quanti numeri vuoi sommare?\n";
         cin >> quanti_num; // aggiorniamo quanti numeri
         
@@ -29,8 +29,13 @@ int main(){
         cout << "Somma numeri. Inserisci numeri uno alla volta\n";
         
         while(cin >> numero){
+            
+            
             numeri.push_back(numero); // inseriamo nel vector i numeri
             }
+        if(quanti_num>numeri.size()){
+            error("Non ci sono abbastanza numeri");
+        }
         if(!cin){
             // inseriamo !cin fuori dal ciclo cosi in caso di input errato ciclo termina e
             //viene controllata la condizione
@@ -43,6 +48,11 @@ int main(){
         }
         double risultato = somma_num(numeri, quanti_num); // var che contiene risultato della somma
         cout << "Il risultato della somma dei " << quanti_num << " primi numeri è: " << risultato << '\n';
+        cout << "Il risultato delle differenze sono:\n";
+        calcola_diff(numeri, differenze); //calcoliamo le differenze e poniamole nel vector differenze e stampiamo
+        for(int i = 0; i<differenze.size(); ++i){
+            cout << differenze.at(i) << ' ';
+        }
         }
     
     
@@ -61,7 +71,7 @@ void error(string s){
     throw runtime_error{s};
 }
 
-double somma_num(vector<double> n, int quanti_num){
+double somma_num(const vector<double>& n, int quanti_num){
     // funzione che restituisce la somma di quanti num nel vector
     // argomenti se vector vuoto errore
     // argomenti se quanti num > vector.size() errore
@@ -85,4 +95,16 @@ double somma_num(vector<double> n, int quanti_num){
         //error("Valore troppo grande o errato");
     //}
     return somma_d; // restituiamo risultato
+}
+
+void calcola_diff(const vector<double>& n, vector<double>& diff){
+    // se n.size()< 2 errore non ci sono abbastanza numeri
+    if(n.size()<2){
+        error("Non ci sono abbastanza numeri da sottrarre.");
+    }
+    for(int i = 1; i<n.size(); ++i){
+        double d = n.at(i)-n.at(i-1); // valore attuale meno il precedente
+        diff.push_back(d); // salviamo la differenza nel vector differenze
+    }
+    
 }
